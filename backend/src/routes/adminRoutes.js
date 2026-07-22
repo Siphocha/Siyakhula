c1b56e32493cc167b7ef331a0a47b1fb84a33ff5
Siphocha <siphochakhala@gmail.com>
2026-07-22 22:23:19 +0200

    my bad

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,5 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const { authenticate } = require('../middleware/authMiddleware');
+const { authorize } = require('../middleware/roleMiddleware');
 const { toggleOracle, getOracleStatus } = require('../controllers/adminController');
-//getting imports.
 
 router.post('/oracle/toggle', authenticate, authorize('admin'), toggleOracle);
 router.get('/oracle/status', authenticate, authorize('admin'), getOracleStatus);
+
+module.exports = router;
