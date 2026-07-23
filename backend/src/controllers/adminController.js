7b007027ad9f364708831d1548d55afda2449aee
Siphocha <siphochakhala@gmail.com>
2026-07-23 13:18:38 +0200

    only touched the controller a few times. THIS IS IT!

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -1,17 +1,13 @@
-const { toggleOracle, getStatus } = require('../services/oracleService');
+const { toggleOracle: toggleOracleService, getStatus } = require('../services/oracleService');
 
 exports.toggleOracle = (req, res) => {
+  console.log('🔁 Toggle oracle called, body:', req.body);
   const { enabled } = req.body;
   if (enabled === undefined) {
     return res.status(400).json({ error: 'enabled flag required' });
   }
-  const newStatus = toggleOracle(enabled);
+  const newStatus = toggleOracleService(enabled);
   res.json({ enabled: newStatus });
-};
-
-exports.toggleOracle = (req, res) => {
-  console.log('🔁 Toggle oracle called, body:', req.body);
-  //debug helperr
 };
 
 exports.getOracleStatus = (req, res) => {
